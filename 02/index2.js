d12861c0543fd2875b00e83728b718a723b44d46
Nicholas Konzen <nicholaskonzen@gmail.com>
2021-12-01 21:23:32 -0800

    solution

--- 02/index2.js
+++ 02/index2.js
@@ -2,21 +2,24 @@ const data = require("./data");
 
 const map = {
     depth: 0,
-    forward: 0
+    forward: 0,
+    aim: 0
 };
 
 for (let dir of data) {
     switch (dir[0]) {
         case "forward":
             map.forward += +dir[1];
+            map.depth += map.aim * +dir[1]
             break;
         case "up":
-            map.depth -= +dir[1];
+            map.aim -= +dir[1];
             break;
         case "down":
-            map.depth += +dir[1];
+            map.aim += +dir[1];
             break;
         default: break;
     }
 };
+
 console.log(map.depth * map.forward);
